f8069021c9b24d492bb40d30c9499f938587b661
Jessica Parsons <verythorough@gmail.com>
2020-10-28 00:24:58 -0700

    Make it slow

--- functions/hello-background.js
+++ functions/hello-background.js
@@ -1,7 +1,4 @@
 exports.handler = async function(event, context) {
-    console.log("called hello")
-    return {
-        statusCode: 200,
-        body: JSON.stringify({message: "Hello World"})
-    };
+    console.log("Hello from the background")
+    setTimeout(() => {  console.log("Goodbye!"); }, 20000);
 }
