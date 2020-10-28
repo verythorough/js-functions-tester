76cce16551ce22de85f4af35e55749cde655cf5e
Jessica Parsons <verythorough@gmail.com>
2020-10-28 11:58:45 -0700

    Add a synchronous return for testing

--- functions/hello-background.js
+++ functions/hello-background.js
@@ -1,4 +1,8 @@
 exports.handler = async function(event, context) {
     console.log("Hello from the background")
     setTimeout(() => {  console.log("Goodbye!"); }, 25000);
+    return {
+        statusCode: 200,
+        body: JSON.stringify({message: "Maybe this was synchronous after all"})
+    };
 }
